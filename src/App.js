a117093efcdc9aa8287d709ddf14efdd411f93da
Aniket <aniketmotwani52@gmail.com>
2023-01-09 01:45:27 +0530

    pushing_all_the_changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+  const [contact,setContact] = useState({
+    fName : "",
+    lName : "",
+    mailID : ""
+  });
+
+  function handleChange(event)
+  {
+    const {name, value} =event.target;
+
+    setContact((prevValue) => {
+      if(name==="fName")
+      {
+        return {
+        fName : value,
+        lName : prevValue.lName,
+        mailID : prevValue.mailID
+        }
+      }
+      else if(name==="lName")
+      {
+        return {
+        fName : prevValue.fName,
+        lName : value,
+        mailID : prevValue.mailID
+        }
+      }
+      else if(name==="email")
+      {
+        return {
+        fName : prevValue.fName,
+        lName : prevValue.lName,
+        mailID : value
+        }
+      }
+    });
+    
+  }
+  return(
+
+    
+    <div className="container">
+    <h1> Hello {contact.fName} {contact.lName}</h1>
+    <p>{contact.mailID}</p>
+      <form>
+          <input name="fName" value={contact.fName} type="text" placeholder="Your First Name" onChange={handleChange} ></input>
+          <input name="lName" value={contact.lName} type="text" placeholder="Your Last Name" onChange={handleChange}></input>
+          <input name="email" value={contact.mailID} type="text" placeholder="Your Email Address" onChange={handleChange}></input>
+
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
